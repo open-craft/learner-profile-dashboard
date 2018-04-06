eb02a5f1e965ffb951cada88ad822a6b782e0570
Tim Krones <tim@opencraft.com>
2018-04-06 12:00:56 +0200

    Don't allow learners to select more than `max_options_to_select` for MRQs.

--- app/static/js/lpd.js
+++ app/static/js/lpd.js
@@ -4,6 +4,30 @@
 
 $(document).ready(function() {
 
+    // Functions
+
+    var updateOptionGroups = function() {
+        var $optionGroups = $('.mr-options');
+        $optionGroups.each(function(i, optionGroup) {
+            disableOptions($(optionGroup));
+        });
+    };
+
+    var disableOptions = function($optionGroup) {
+        var maxOptionsToSelect = $optionGroup.data('max-options-to-select'),
+            $options = $optionGroup.find('.mr-option'),
+            $selectedOptions = $options.filter(':checked'),
+            $unselectedOptions = $options.not(':checked');
+
+        if ($selectedOptions.length >= maxOptionsToSelect) {
+            $unselectedOptions.attr('disabled', true);
+        } else {
+            $unselectedOptions.attr('disabled', false);
+        }
+
+    };
+
+
     // Event handlers
 
     $('.section-title').click(function(e) {
@@ -14,5 +38,20 @@ $(document).ready(function() {
         $questions.toggle();
         $controls.toggle();
     });
+
+    $('.mr-option').click(function(e) {
+        var $optionGroup = $(this).parents('.mr-options');
+        disableOptions($optionGroup);
+    });
+
+
+    // Initialization
+
+    var init = function() {
+        console.log('Initializing ...');
+        updateOptionGroups();
+    };
+
+    init();
 
 });

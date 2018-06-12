6fdbf8faf707bccdb521006077d7a4c056a18f89
Tim Krones <tim@opencraft.com>
2018-06-12 12:07:18 +0200

    Add rotating arrows to section titles.

--- app/static/js/lpd.js
+++ app/static/js/lpd.js
@@ -179,10 +179,13 @@ $(document).ready(function() {
 
     $('.section-title').click(function(e) {
         var $sectionForm = $(this).parent('form'),
+            $sectionCollapse = $sectionForm.find('.section-collapse'),
             $intro = $sectionForm.children('.section-intro'),
             $questions = $sectionForm.children('.section-questions'),
             $controls = $sectionForm.children('.section-controls');
 
+        $sectionCollapse.toggleClass('expanded');
+        $sectionCollapse.toggleClass('collapsed');
         $intro.toggle('slow');
         $questions.toggle('slow');
         $controls.toggle('slow');

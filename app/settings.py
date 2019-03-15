e0a325a5da849cb10c0d67cff91974c032ab43b2
Tim Krones <tim@opencraft.com>
2019-03-15 15:07:43 +0100

    Remove redundant TEMPLATES definition.

--- app/settings.py
+++ app/settings.py
@@ -53,22 +53,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'app.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'app.wsgi.application'
 
 # Session cookie: use a different name from edx

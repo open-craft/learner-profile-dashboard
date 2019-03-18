3f973660452bbaf0344f7101db743d87c03c1048
Tim Krones <tim@opencraft.com>
2019-03-18 12:10:55 +0100

    Reorder default middleware classes to match default order in Django 1.11.

--- app/settings.py
+++ app/settings.py
@@ -41,12 +41,12 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'iframe.middleware.iframe_middleware.IFrameFixMiddleware',
 ]
 

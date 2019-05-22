b7f86654ff1f4eec5a374c0a9315d53d0b911925
Tim Krones <tim@opencraft.com>
2019-05-22 12:10:04 +0200

    Collapse sections by default.

--- app/static/js/section.js
+++ app/static/js/section.js
@@ -4,20 +4,40 @@
 
 $(document).ready(function() {
 
-    // Event handlers
+    // Globals
 
-    $('.section-title').click(function(e) {
-        var $sectionForm = $(this).parent('form'),
+    var RET = 13;
+    var SPC = 32;
+
+    // Functions
+
+    var toggleSection = function($section) {
+        var $sectionForm = $section.parent('form'),
             $sectionCollapse = $sectionForm.find('.section-collapse'),
             $intro = $sectionForm.children('.section-intro'),
             $questions = $sectionForm.children('.section-questions'),
             $controls = $sectionForm.children('.section-controls');
 
-        $sectionCollapse.toggleClass('expanded');
         $sectionCollapse.toggleClass('collapsed');
+        $sectionCollapse.toggleClass('expanded');
         $intro.toggle('slow');
         $questions.toggle('slow');
         $controls.toggle('slow');
+    };
+
+    // Event handlers
+
+    $('.section-title').on('click', function(e) {
+        var $section = $(this);
+        toggleSection($section);
+    });
+
+    $('.section-title').on('keydown', function(e) {
+        if (e.which === RET || e.which === SPC) {
+            e.preventDefault();
+            var $section = $(this);
+            toggleSection($section);
+        }
     });
 
 });
